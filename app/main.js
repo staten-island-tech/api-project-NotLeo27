af1d80a63c20484795183db0ccdee7bcaf13e583
NotLeo27 <158211664+NotLeo27@users.noreply.github.com>
2024-11-25 11:24:36 -0500

    we ball

--- app/main.js
+++ app/main.js
@@ -1,1 +1,21 @@
 import "./style.css";
+
+async function getData() {
+  //fetch returns a promise
+
+  try {
+    const response = await fetch(/* URL */);
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      document.querySelector("h1").textContent = data.name;
+    }
+  } catch (error) {
+    console.log(error);
+    alert("sorry could not find that pocket onster");
+  }
+
+  console.log(response);
+  const data = await response.json();
+}
